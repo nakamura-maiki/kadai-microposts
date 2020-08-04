f4b52f0c8801d9f353ea2050e0ba62b2c8759e41
siratama <565siratama@gmail.com>
2020-08-04 02:35:45 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.app')
 
-@section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+@section("content")
+    @if(Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route("signup.get", "Sign up now!", [], ["class" => "btn btn-lg btn-primary"]) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
+
+
+
+
+<!--@section('content')-->
+<!--    <div class="center jumbotron">-->
+<!--        <div class="text-center">-->
+<!--            <h1>Welcome to the Microposts</h1>-->
+<!--            {{-- ユーザ登録ページへのリンク --}}-->
+<!--            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}-->
+<!--        </div>-->
+<!--    </div>-->
+<!--@endsection-->
