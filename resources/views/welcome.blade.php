6692b425b38ecd1ebcc34503962162af58e3a231
siratama <565siratama@gmail.com>
2020-08-04 06:01:54 +0000

    user pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,14 +15,3 @@
 @endsection
 
 
-
-
-<!--@section('content')-->
-<!--    <div class="center jumbotron">-->
-<!--        <div class="text-center">-->
-<!--            <h1>Welcome to the Microposts</h1>-->
-<!--            {{-- ユーザ登録ページへのリンク --}}-->
-<!--            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}-->
-<!--        </div>-->
-<!--    </div>-->
-<!--@endsection-->
